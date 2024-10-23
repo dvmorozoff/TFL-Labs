6ba8cee5f79e724bd64daf8aa774e08fd8ad52e7
Морозов Дмитрий <kretovskiyd@gmail.com>
2024-10-24 01:31:54 +0300

    api

--- Lab2/main2.py
+++ Lab2/main2.py
@@ -9,6 +9,7 @@ response = requests.post('http://localhost:8080/generate', json=payload)
 def membership(word):
     payload = {"word": word}
     response = requests.post('http://localhost:8080/checkWord', json=payload)
+    #response = requests.post('http://localhost:8095/checkWord', json=payload)
     if response.status_code == 200:
         result = response.json()
         return result.get("response", None)
@@ -21,34 +22,36 @@ def equivalence(class_table, suffixes):
     ssuffixes = ' '.join(suffixes)
     table = ''
     lower_prefixes = ''
-    for count_pref in range(len(upper_prefixes)):
-        for count_low_pref in range(len(class_table[upper_prefixes[count_pref]])):
-            if count_low_pref != 0:
-                lower_prefixes = lower_prefixes + ' ' + class_table[upper_prefixes[count_pref]][count_low_pref]
-
+    #print('upper_prefixes:', upper_prefixes)
+    #print('suffixes:', suffixes)
     for count_pref in range(len(upper_prefixes)):
         for count_suf in range(len(suffixes)):
-            word = prefixes[count_pref] + suffixes[count_suf]
+            word = upper_prefixes[count_pref] + suffixes[count_suf]
             #word = word.replace('ε', '')
-            table = table + ' ' + str(membership(word))
-    if main_prefixes == '':
-        main_prefixes = 'ε '
-    if main_prefixes[0] != 'ε':
-        main_prefixes = 'ε ' + main_prefixes[1:]
-    if ssuffixes == ' ':
-        ssuffixes = 'ε'
-    print('______________________')
-    print('suffixes:', ssuffixes)
-    print('prefixes:', main_prefixes)
-    print(table)
+            check_word = membership(word)
+            table = table + ' ' + str(check_word)
+            #print('мемьершип в эквивалентности:')
+            #print(upper_prefixes[count_pref], ':', suffixes[count_suf], ':', check_word)
+    #if main_prefixes == '':
+    #    main_prefixes = ' '
+    #if main_prefixes[0] != 'ε':
+    main_prefixes = 'ε' + main_prefixes
+    ssuffixes = 'ε' + ssuffixes
+    #print('______________________')
+    #print('suffixes:', ssuffixes)
+    #print('upper prefixes:', main_prefixes)
+    if main_prefixes == ssuffixes and ssuffixes == 'ε':
+        table = str(membership('ε'))
+    #print(table)
     #print('low_pref:', lower_prefixes)
     payload = {
         "main_prefixes": main_prefixes,
-        "non_main_prefixes": " ",  # Пустой объект
+        "non_main_prefixes": "",  # Пустой объект
         "suffixes": ssuffixes,  # Элемент e
         "table": table
     }
     response = requests.post('http://localhost:8080/checkTable', json=payload)
+    #response = requests.post('http://localhost:8095/checkTable', json=payload)
     print('Статус-код ответа:', response.status_code)
     print('Текст ответа:', response.text)
     if response.status_code == 200:
@@ -56,7 +59,7 @@ def equivalence(class_table, suffixes):
         response_value = result.get("response", None)
         type_value = result.get("type", None)
         print('_____________________________________')
-        print('type_value:', type_value, response_value)
+        return response_value
         if type_value is True:
             return response_value
         elif type_value is False:
@@ -70,47 +73,12 @@ def equivalence(class_table, suffixes):
         print('Ошибка эквивалентность')
         return None
 
-'''def equivalence(file_name, matrix_in_mat):
-    main_prefixes = ' '.join(row[0] for row in matrix_in_mat)
-    table = ' '.join(str(row[1]) for row in matrix_in_mat)
-    suffixes = ' '.join(str(row[2]) for row in matrix_in_mat)
-    #print(main_prefixes)
-    #print(suffixes)
-    #print(table)
-    payload = {
-        "main_prefixes": main_prefixes,
-        "non_main_prefixes": "",  # Пустой объект
-        "suffixes": suffixes,  # Элемент e
-        "table": table
-    }
-    response = requests.post('http://localhost:8080/checkTable', json=payload)
-    print('Статус-код ответа:', response.status_code)
-    print('Текст ответа:', response.text)
-    if response.status_code == 200:
-        result = response.json()
-        response_value = result.get("response", None)
-        type_value = result.get("type", None)
-        print('_____________________________________')
-        print('type_value:', type_value, response_value)
-        if type_value is True:
-            return response_value
-        elif type_value is False:
-            return response_value
-        elif type_value is None:
-            return 'true'
-        else:
-            print('Неизвестное значение type')
-            return None
-    else:
-        print('Ошибка эквивалентность')
-        return None
-'''
 #print('Введите название файла с тестовыми данными с расширением:')
 #file_name = input()
 file_name = '092.txt'
 with open(file_name, 'r') as file:
-    e = 'ε'  # Возможность задать epsilon по-разному
-    #e = ' '
+    #e = 'ε'  # Возможность задать epsilon по-разному
+    e = ''
     alphabet = []
     alphabet.append(e)
     stringalphabet = file.readline().strip()
@@ -131,7 +99,8 @@ with open(file_name, 'r') as file:
         word = alphabet[number_elem_in_alph] + suffixes[0]
         if word == 'εε':
             word = 'ε'
-        word = word.replace('ε', '')
+        if word != 'ε':
+            word = word.replace('ε', '')
         check_word = membership(word)  # true or False
         count_of_lines += 1
         print(alphabet[number_elem_in_alph], suffixes[0], ':', check_word, ':', count_of_lines)
@@ -197,31 +166,23 @@ with open(file_name, 'r') as file:
     #--------------------------------------------------------------------
     matrix_in_mat = [['', '', ''] for i in range(count_upper)]
     upper_prefixes = list(class_table.keys())
-    #Заполнение таблицы, которую переводят в МАТ
-    '''for number_of_up_pref in range(count_upper):
-        matrix_in_mat[number_of_up_pref][0] = upper_prefixes[number_of_up_pref]
-        if (class_table[upper_prefixes[number_of_up_pref]][0] == 'T'):
-            matrix_in_mat[number_of_up_pref][1] = 1
-        else:
-            matrix_in_mat[number_of_up_pref][1] = 0
-        matrix_in_mat[number_of_up_pref][2] = ' '''''
     previous_count_suf = 1
     count_elems_in_table = count_upper - 1
     counterexample = equivalence(class_table, suffixes)
     while counterexample != 'true':
         #Добавление новых суффиксов
-        #for row in matrix_in_mat:  # Вывод таблицы для мата
-        #    print("{:>5} {:>5}".format(*row))
-        #print(counterexample)
         for suffix_iterator in range(len(counterexample)):
             new_suffix = counterexample[len(counterexample) - suffix_iterator - 1:len(counterexample)]
-            check_already_add = True
+            check_already_add_suf = True
             for number_of_suf in range(len(suffixes)):
                 if new_suffix == suffixes[number_of_suf]:
-                    check_already_add = False
+                    check_already_add_suf = False
                     break
-            if check_already_add:
+            if check_already_add_suf:
                 suffixes.append(new_suffix)
+        new_suffix = counterexample
+        class_table[new_suffix] = ['T']
+
         #Построение продолжения таблицы к. э.
         for number_of_suf in range(previous_count_suf, len(suffixes)):
             for number_of_up_pref in range(len(upper_prefixes)):
@@ -238,10 +199,6 @@ with open(file_name, 'r') as file:
                     table_row = class_table[upper_prefixes[number_of_up_pref]][0]
                     #Добавление элемента в таблицу для МАТ'а
                     count_elems_in_table += 1
-                    '''matrix_in_mat.append(['', '', ''])
-                    matrix_in_mat[count_elems_in_table][0] = upper_prefixes[number_of_up_pref]
-                    matrix_in_mat[count_elems_in_table][1] = check_word
-                    matrix_in_mat[count_elems_in_table][2] = suffixes[number_of_suf]'''
                     delete_elems = []
                     #2 варианта в зависимости от результата membership
                     if check_word:
@@ -263,19 +220,7 @@ with open(file_name, 'r') as file:
                                     new_word_class = prefix
                                     delete_elems.append(new_word_class)
                                     upper_prefixes.append(prefix)
-                                    #class_table[upper_prefixes[number_of_up_pref]].remove(new_word_class)
-                                    #Добавление новых элементов таблицы для МАТ'а
-                                    #с учётом появления нового префикса
-                                    '''for suffix_iterator in range(number_of_suf + 1):
-                                        count_elems_in_table += 1
-                                        matrix_in_mat.append(['', '', ''])
-                                        matrix_in_mat[count_elems_in_table][0] = prefix
-                                        matrix_in_mat[count_elems_in_table][2] = suffixes[suffix_iterator]
-                                        table_row_string = class_table[prefix][0]
-                                        if table_row_string[suffix_iterator] == 'T':
-                                            matrix_in_mat[count_elems_in_table][1] = 1
-                                        else:
-                                            matrix_in_mat[count_elems_in_table][1] = 0'''
+
                                     #Расширение префиксов
                                     for symbol in range(1, len(alphabet)):
                                         new_prefix = prefix + alphabet[symbol]
@@ -298,15 +243,6 @@ with open(file_name, 'r') as file:
                                         if check_equal_new_pref:
                                             class_table[new_prefix] = [table_row_new_prefix]
                                             upper_prefixes.append(new_prefix)
-                                            '''for suffix_iterator in range(1, number_of_suf + 1):
-                                                count_elems_in_table += 1
-                                                matrix_in_mat.append(['', '', ''])
-                                                matrix_in_mat[count_elems_in_table][0] = new_prefix
-                                                matrix_in_mat[count_elems_in_table][2] = suffixes[suffix_iterator]
-                                                if table_row_new_prefix[suffix_iterator] == 'T':
-                                                    matrix_in_mat[count_elems_in_table][1] = 1
-                                                else:
-                                                    matrix_in_mat[count_elems_in_table][1] = 0'''
                                 else:
                                     class_table[new_word_class].append(prefix)
                                     delete_elems.append(prefix)
@@ -329,16 +265,6 @@ with open(file_name, 'r') as file:
                                     new_word_class = prefix
                                     delete_elems.append(new_word_class)
                                     upper_prefixes.append(prefix)
-                                    '''for suffix_iterator in range(number_of_suf + 1):
-                                        matrix_in_mat.append(['', '', ''])
-                                        count_elems_in_table += 1
-                                        matrix_in_mat[count_elems_in_table][0] = prefix
-                                        matrix_in_mat[count_elems_in_table][2] = suffixes[suffix_iterator]
-                                        table_row_string = class_table[prefix][0]
-                                        if table_row_string[suffix_iterator] == 'T':
-                                            matrix_in_mat[count_elems_in_table][1] = 1
-                                        else:
-                                            matrix_in_mat[count_elems_in_table][1] = 0'''
                                     #Расширение префиксов
                                     for symbol in range(1, len(alphabet)):
                                         new_prefix = prefix + alphabet[symbol]
@@ -361,15 +287,6 @@ with open(file_name, 'r') as file:
                                         if check_equal_new_pref:
                                             class_table[new_prefix] = [table_row_new_prefix]
                                             upper_prefixes.append(new_prefix)
-                                            '''for suffix_iterator in range(number_of_suf + 1):
-                                                count_elems_in_table += 1
-                                                matrix_in_mat.append(['', '', ''])
-                                                matrix_in_mat[count_elems_in_table][0] = new_prefix
-                                                matrix_in_mat[count_elems_in_table][2] = suffixes[suffix_iterator]
-                                                if table_row_new_prefix[suffix_iterator] == 'T':
-                                                    matrix_in_mat[count_elems_in_table][1] = 1
-                                                else:
-                                                    matrix_in_mat[count_elems_in_table][1] = 0'''
                                 else:
                                     class_table[new_word_class].append(prefix)
                                     delete_elems.append(prefix)
